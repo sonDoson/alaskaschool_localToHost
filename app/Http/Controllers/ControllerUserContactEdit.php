d317e517bc2f6e39aa3bc40b1ba6892ac188dcfd
sonDoson <xulazy@gmail.com>
2018-11-09 16:09:44 +0700

    contact

--- app/Http/Controllers/ControllerUserContactEdit.php
+++ app/Http/Controllers/ControllerUserContactEdit.php
@@ -27,8 +27,12 @@ class ControllerUserContactEdit extends Controller
         $total_value[1]['value'] = $request->youtube;
         $total_value[2]['name'] = 'instagram';
         $total_value[2]['value'] = $request->instagram;
+        //map
+        $str_map = $request->map;
+        $str_map = explode(" ",$str_map);
+        
         $total_value[3]['name'] = 'map';
-        $total_value[3]['value'] = $request->map;
+        $total_value[3]['value'] = $str_map[1];
         foreach($total_value as $value){
             if($value['value'] !== null){
                 DB::table('contact')->where('id', 1)->update([$value['name'] => $value['value']]);
--- resources/views/client/content/contact.blade.php
+++ resources/views/client/content/contact.blade.php
@@ -1,8 +1,13 @@
 @extends('client.layout.contact_layout')
 @section('content')
     <div style="display: grid; grid-template-columns: auto 300px;grid-column-gap: 20px;">
-    <div class="font-resize">
-
+    <div class="font-resize" style="width: 100%;display: grid; grid-template-rows: auto 250px; grid-row-gap: 20px">
+        <div style="border: 1px solid black;">
+        {{ $contact[$lang[1]] }}
+        </div>
+        <div style="bottom: 0;display:block;width: 600px; height: 100%; margin: 0 auto;">
+        <iframe {!! $contact['map'] !!} width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
     </div>
     </div>
 @stop
